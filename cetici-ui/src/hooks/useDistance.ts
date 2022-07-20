5c4416afb2b6bb3f14f00eb4014d813554610cb0
wimpheling <wimpheling@gmail.com>
2022-07-20 01:39:43 +0100

    fix distance

--- cetici-ui/src/hooks/useDistance.ts
+++ cetici-ui/src/hooks/useDistance.ts
@@ -2,8 +2,8 @@ export const useDistanceFormatter = (distanceInMeters: number) => {
   if (distanceInMeters < 1000) {
     return `${Math.round(distanceInMeters)}m`;
   } else if (distanceInMeters < 10000) {
-    return `${distanceInMeters.toFixed(2)}km`;
+    return `${(distanceInMeters / 1000).toFixed(2)}km`;
   } else {
-    return `${Math.round(distanceInMeters)}km`;
+    return `${Math.round(distanceInMeters / 1000)}km`;
   }
 };
